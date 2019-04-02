baf76bc12d99658b671c5056785e4f5492a92f58
woojae9488 <woojae9488@naver.com>
2019-04-02 10:19:43 +0900

    Add Tt_html [NodeJS - condition]

--- NodeJS/main.js
+++ NodeJS/main.js
@@ -4,17 +4,39 @@ var url = require('url');
 
 var app = http.createServer(function (request, response) {
     var _url = request.url;
-    var queryDate = url.parse(_url, true).query;
-    if (_url == '/') {
-        _url = '/index.html';
-    }
-    if (_url == '/favicon.ico') {
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + _url));
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    var title = queryData.id;
+    if (pathname === '/') {
+        fs.readFile(`Data/${title}`, 'utf-8', function (err, description) {
+            var template = `
+    <!doctype html>
+<html>
 
+<head>
+  <title>WEB1 - ${title}</title>
+  <meta charset="utf-8">
+</head>
+
+<body>
+  <h1><a href="/">WEB</a></h1>
+  <ol>
+    <li><a href="/?id=HTML">HTML</a></li>
+    <li><a href="/?id=CSS">CSS</a></li>
+    <li><a href="/?id=JavaScript">JavaScript</a></li>
+  </ol>
+  <h2>${title}</h2>
+  <p>${description}</p>
+</body>
+
+</html>
+    `;
+            response.writeHead(200);
+            response.end(template);
+        });
+    } else {
+        response.writeHead(404);
+        response.end('Not found');
+    }
 });
 app.listen(3000);
