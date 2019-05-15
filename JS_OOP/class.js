e880adac980190449e4a0f95d1d4da262b8f4eaf
woojae9488 <woojae9488@naver.com>
2019-05-15 22:17:38 +0900

    Add Tt_html [JS_OOP end]

--- JS_OOP/class.js
+++ JS_OOP/class.js
@@ -5,15 +5,22 @@ class Person {
         this.second = second;
     }
     sum() {
-        return 'prototype: ' + (this.first + this.second);
+        return this.first + this.second;
+    }
+}
+class PersonPlus extends Person {
+    constructor(name, first, second, third) {
+        super(name, first, second);
+        this.third = third
+    }
+    sum() {
+        return super.sum() + this.third;
+    }
+    avg() {
+        return (this.first + this.second + this.third) / 3;
     }
 }
 
-
-var kim = new Person('kim', 10, 20);
-kim.sum = function () {
-    return 'this : ' + (this.first + this.second);
-}
-var lee = new Person('lee', 10, 10);
+var kim = new PersonPlus('kim', 10, 20, 30);
 console.log("kim.sum()", kim.sum());
-console.log("lee.sum()", lee.sum());
+console.log("kim.avg()", kim.avg());
---
+++ JS_OOP/object_function.js
@@ -0,0 +1,11 @@
+var kim = { name: 'kim', first: 10, second: 20 }
+var lee = { name: 'lee', first: 10, second: 10 }
+function sum(prefix) {
+    return prefix + (this.first + this.second);
+}
+// sum();
+console.log('sum.call(kim)', sum.call(kim, '=> ')); //apply
+console.log('sum.call(lee)', sum.call(lee, ': '));
+
+var kimSum = sum.bind(kim, '-> ');
+console.log('kimSum()', kimSum());
